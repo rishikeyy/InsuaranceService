571cf30766fa998dd7d36efddfadeff701184f44
_bidhayak <rishikeyyadav@gmail.com>
2025-08-16 14:36:49 +0530

    debugged test

--- claimService/claim/src/test/java/com/example/claimService/demo/unitTest/ClaimServiceTest.java
+++ claimService/claim/src/test/java/com/example/claimService/demo/unitTest/ClaimServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.claimService.demo.unitTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import domain.Service.ClaimService;
 import domain.Service.EventPublisher;
 import domain.model.ClaimDto;
@@ -20,29 +22,40 @@ public class ClaimServiceTest {
     @InjectMocks
     ClaimService claimService;
 
-    String claimJson="{\n" +
-            "  \"claimId\": \"CLM123456\",\n" +
-            "  \"policyNumber\": \"POL987654\",\n" +
-            "  \"claimType\": \"Health\",\n" +
-            "  \"claimAmount\": 15000.75,\n" +
-            "  \"incidentDescription\": \"Hospitalization due to minor surgery\",\n" +
-            "  \"incidentDate\": \"2025-08-10\",\n" +
-            "  \"reportedDate\": \"2025-08-12\",\n" +
-            "  \"contactEmail\": \"john.doe@example.com\",\n" +
-            "  \"contactPhone\": \"+919876543210\"\n" +
-            "}";
-    ObjectMapper mapper = new ObjectMapper();
+    String claimJson= """
+            {
+              "claimId": "CLM123456",
+              "policyNumber": "POL987654",
+              "claimType": "Health",
+              "claimAmount": 15000.75,
+              "incidentDescription": "Hospitalization due to minor surgery",
+              "incidentDate": "2025-08-10",
+              "reportedDate": "2025-08-12",
+              "contactEmail": "john.doe@example.com",
+              "contactPhone": "+919876543210"
+            }""";
+    //ObjectMapper mmapper = new ObjectMapper();
+    ObjectMapper mapper = JsonMapper.builder()
+            .findAndAddModules()
+            .build();
     /*
     Note: java can handle parsing JSON into simple types like(string,int etc.),
     But, it can't handle conversion of LocalDate objects... so to handle it java provides modules like "jackson-datatype-jsr310"
     To register it use mapper.registerModule(new JavaTimeModule()) OR mapper.findAndRegisterModules();
     */
-    mapper.registerModule(new JavaTimeModule());
 
-    ClaimDto claimDto=mapper.readValue(claimJson, ClaimDto.class);
+    //mmapper.registerModule(new void JavaTimeModule());
+
+
 
     @Test
-    void submitClaim(){
-
+    void submitClaim() throws JsonProcessingException {
+        try {
+            ClaimDto claimDto=mapper.readValue(claimJson, ClaimDto.class);
+            claimService.SubmitClaim(claimDto);
+//            return mapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting DTO to JSON", e);
+        }
     }
 }
