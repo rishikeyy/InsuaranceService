ff1c4481c2f73359a09bb724f874578a2ba0b70e
rishikeyy <rishikeyyadav@gmail.com>
2026-05-23 19:27:08 +0530

    modified insurance main class

--- insuranceService/src/main/java/com/example/insuranceservice/InsuranceServiceApplication.java
+++ insuranceService/src/main/java/com/example/insuranceservice/InsuranceServiceApplication.java
@@ -1,3 +1,15 @@
+package com.example.insuranceservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class InsuranceServiceApplication {
-    
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+            InsuranceServiceApplication.class,
+            args
+        );
+    }
 }
